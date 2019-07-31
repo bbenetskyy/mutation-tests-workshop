0b2bb09b712f734485bd6b6ecb6c92c1cc33e8f1
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2019-07-31 10:24:14 +0200

    #3 Converter Part 3

---
+++ Initial Solution/RzeszowBusCore.Tests/BaseJsonToObjectConverterTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FluentAssert;
+using FluentAssertions;
+using Newtonsoft.Json;
+using RzeszowBusCore.Services;
+using RzeszowBusCore.Tests.TestData;
+using Xunit;
+
+namespace RzeszowBusCore.Tests
+{
+    public class BaseJsonToObjectConverterTests
+    {
+        [Fact]
+        public void Test()
+        {
+            // Arrange
+            var converter = new BaseJsonToObjectConverter();
+            var filePath = "./TestData/TestClass.json";
+
+            // Act
+            var jsonString = File.ReadAllText(filePath);
+            var testClass = converter.Convert<ConverterTestClass>(jsonString);
+
+            // Assert
+            testClass.Should().BeEquivalentTo(ConverterTestData.TestClass);
+        }
+    }
+
+    public class ConverterTestClass
+    {
+        public int @Int { get; set; }
+        public long @Long { get; set; }
+        public string @String { get; set; }
+        public DateTime @DateTime { get; set; }
+        public TimeSpan @TimeSpan { get; set; }
+        public TestEnum @Enum { get; set; }
+    }
+
+    public enum TestEnum
+    {
+        Fake, Test
+    }
+}
---
+++ Initial Solution/RzeszowBusCore.Tests/TestData/ConverterTestData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RzeszowBusCore.Tests.TestData
+{
+    public static class ConverterTestData
+    {
+        public static ConverterTestClass TestClass { get; }
+
+        static ConverterTestData()
+        {
+            TestClass = new ConverterTestClass
+            {
+                DateTime = Convert.ToDateTime("7/31/2019"),
+                TimeSpan = Convert.ToDateTime("12:00").TimeOfDay,
+                Enum = TestEnum.Test,
+                Int = 10,
+                Long = 100,
+                String = "String"
+            };
+        }
+    }
+}
